735f1de9e70c9974a5106d9a34af42a9295259fe
Rastavelli <rashid.zhauzhurek@gmail.com>
2018-01-05 00:08:37 +0600

    add src/index.js even game logic

--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
 import readlineSync from 'readline-sync';
 
-const printPlayerName = () => {
-  const playerName = readlineSync.question('May I have your name?');
+const printGreetings = () => console.log('Welcome to the Brain Games!');
+
+const even = () => {
+  printGreetings();
+
+  console.log('Answer "yes" if number even otherwise answer "no".');
+
+  const playerName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${playerName}`);
+
+  for (let i = 0; i < 3; i += 1) {
+    const number = Math.floor(Math.random() * 101);
+    const rightAnswer = number % 2 === 0 ? 'yes' : 'no';
+    console.log(`Question: ${number}`);
+    const playerAnswer = readlineSync.question('Your answer: ');
+
+    if (rightAnswer === playerAnswer) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${playerAnswer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.
+Let's try again, ${playerName}!`);
+      return;
+    }
+  }
+  console.log(`Congratulations, ${playerName}!`);
 };
 
-export default printPlayerName;
+export { printGreetings };
+export default even;
